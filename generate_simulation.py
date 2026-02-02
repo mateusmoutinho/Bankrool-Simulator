1b9983b55115bc863301f340846aad1201c53ec4
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-02 01:48:41 -0300

    att

--- generate_simulation.py
+++ generate_simulation.py
@@ -1,5 +1,4 @@
 from src import *
 
 r = play_simulation('config/simulations','config/sessions','config/games','current')
-print(get_game_ev('config/games','Baccarat'))
-print(r)
+print(get_game_ev('config/games','Multi-Table Tournament'))
